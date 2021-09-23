9bfa716585c2a28dba514a201bb3aae182848865
chhunhorn <56245196+chhunhorn@users.noreply.github.com>
2021-09-23 11:18:34 -0500

    Testing Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        MethodA();
+        methodB();
+    }
+
+    public static void MethodA(){
+
+    }
+
+    public static void methodB(){
+
+    }
+}
